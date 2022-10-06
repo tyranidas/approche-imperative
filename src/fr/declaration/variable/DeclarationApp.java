0b8cbf4db070a3a420f6d84e3d03b8a8d0969f2d
Manuel <tyranidas@hotmail.com>
2022-10-06 16:39:58 +0200

    test

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 1;
+		short b = 10;
+		int c = 100;
+		long d = 1000L;
+		float e = 1.2f;
+		double f = 25.25;
+		char g ='g';
+		boolean h = true;
+		String i = "enfin fini";
+		System.out.println(i);
+		String randomString= "Voici le résultat d’un calcul : \r\n  1+5=6";
+		System.out.println(randomString);
+	}
+
+}
